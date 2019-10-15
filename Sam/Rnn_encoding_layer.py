fcaf7e9a8bbf4e9636b6c359b66d833c21920181
Somyajit Chakraborty Sam <somyajitchppr@gmail.com>
2019-10-15 11:55:31 +0530

    Create Rnn_encoding_layer.py
    This is small part of the RNN for seq2seq data feed........

---
+++ Sam/Rnn_encoding_layer.py
@@ -0,0 +1,24 @@
+def encoding_layer(rnn_size, sequence_length, num_layers, rnn_inputs, keep_prob):
+    '''Create the encoding layer'''
+    
+    for layer in range(num_layers):
+        with tf.variable_scope('encoder_{}'.format(layer)):
+            cell_fw = tf.contrib.rnn.LSTMCell(rnn_size,
+                                              initializer=tf.random_uniform_initializer(-0.1, 0.1, seed=2))
+            cell_fw = tf.contrib.rnn.DropoutWrapper(cell_fw, 
+                                                    input_keep_prob = keep_prob)
+
+            cell_bw = tf.contrib.rnn.LSTMCell(rnn_size,
+                                              initializer=tf.random_uniform_initializer(-0.1, 0.1, seed=2))
+            cell_bw = tf.contrib.rnn.DropoutWrapper(cell_bw, 
+                                                    input_keep_prob = keep_prob)
+
+            enc_output, enc_state = tf.nn.bidirectional_dynamic_rnn(cell_fw, 
+                                                                    cell_bw, 
+                                                                    rnn_inputs,
+                                                                    sequence_length,
+                                                                    dtype=tf.float32)
+    # Join outputs since we are using a bidirectional RNN
+    enc_output = tf.concat(enc_output,2)
+    
+    return enc_output, enc_state
