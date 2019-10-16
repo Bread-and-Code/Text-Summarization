6fe3062a15aefe83943ef34c0cc9772bb901acd0
Somyajit Chakraborty Sam <somyajitchppr@gmail.com>
2019-10-16 22:42:34 +0530

    Create misc_batches.py

---
+++ Sam/misc_batches.py
@@ -0,0 +1,72 @@
+def get_batches(summaries, texts, batch_size):
+    """Batch summaries, texts, and the lengths of their sentences together"""
+    for batch_i in range(0, len(texts)//batch_size):
+        start_i = batch_i * batch_size
+        summaries_batch = summaries[start_i:start_i + batch_size]
+        texts_batch = texts[start_i:start_i + batch_size]
+        pad_summaries_batch = np.array(pad_sentence_batch(summaries_batch))
+        pad_texts_batch = np.array(pad_sentence_batch(texts_batch))
+        
+        # Need the lengths for the _lengths parameters
+        pad_summaries_lengths = []
+        for summary in pad_summaries_batch:
+            pad_summaries_lengths.append(len(summary))
+        
+        pad_texts_lengths = []
+        for text in pad_texts_batch:
+            pad_texts_lengths.append(len(text))
+        
+        yield pad_summaries_batch, pad_texts_batch, pad_summaries_lengths, pad_texts_lengths
+
+
+# Set the Hyperparameters
+epochs = 100
+batch_size = 64
+rnn_size = 256
+num_layers = 2
+learning_rate = 0.005
+keep_probability = 0.75
+
+# Build the graph
+train_graph = tf.Graph()
+# Set the graph to default to ensure that it is ready for training
+with train_graph.as_default():
+    
+    # Load the model inputs    
+    input_data, targets, lr, keep_prob, summary_length, max_summary_length, text_length = model_inputs()
+
+    # Create the training and inference logits
+    training_logits, inference_logits = seq2seq_model(tf.reverse(input_data, [-1]),
+                                                      targets, 
+                                                      keep_prob,   
+                                                      text_length,
+                                                      summary_length,
+                                                      max_summary_length,
+                                                      len(vocab_to_int)+1,
+                                                      rnn_size, 
+                                                      num_layers, 
+                                                      vocab_to_int,
+                                                      batch_size)
+    
+    # Create tensors for the training logits and inference logits
+    training_logits = tf.identity(training_logits.rnn_output, 'logits')
+    inference_logits = tf.identity(inference_logits.sample_id, name='predictions')
+    
+    # Create the weights for sequence_loss
+    masks = tf.sequence_mask(summary_length, max_summary_length, dtype=tf.float32, name='masks')
+
+    with tf.name_scope("optimization"):
+        # Loss function
+        cost = tf.contrib.seq2seq.sequence_loss(
+            training_logits,
+            targets,
+            masks)
+
+        # Optimizer
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+
+        # Gradient Clipping
+        gradients = optimizer.compute_gradients(cost)
+        capped_gradients = [(tf.clip_by_value(grad, -5., 5.), var) for grad, var in gradients if grad is not None]
+        train_op = optimizer.apply_gradients(capped_gradients)
+print("Graph is built.")
