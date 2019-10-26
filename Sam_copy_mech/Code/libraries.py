24d4002f032218b78a91348b5f9d48e5c1f118c0
Somyajit Chakraborty Sam <somyajitchppr@gmail.com>
2019-10-26 16:21:17 +0530

    Create libraries.py
    lis of libraries imported for further use in the project

---
+++ Sam_copy_mech/Code/libraries.py
@@ -0,0 +1,13 @@
+import numpy as np  
+import pandas as pd 
+import re           
+from bs4 import BeautifulSoup 
+from keras.preprocessing.text import Tokenizer 
+from keras.preprocessing.sequence import pad_sequences
+from nltk.corpus import stopwords   
+from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate, TimeDistributed, Bidirectional
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+import warnings
+pd.set_option("display.max_colwidth", 200)
+warnings.filterwarnings("ignore")
