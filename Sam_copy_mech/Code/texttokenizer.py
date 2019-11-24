615ecec3e043accf3edec572478e5587a784e359
Somyajit Chakraborty Sam <somyajitchppr@gmail.com>
2019-11-24 18:25:43 +0530

    Create texttokenizer.py

---
+++ Sam_copy_mech/Code/texttokenizer.py
@@ -0,0 +1,13 @@
+#prepare a tokenizer for reviews on training data
+x_tokenizer = Tokenizer()
+x_tokenizer.fit_on_texts(list(x_tr))
+
+#convert text sequences into integer sequences
+x_tr    =   x_tokenizer.texts_to_sequences(x_tr) 
+x_val   =   x_tokenizer.texts_to_sequences(x_val)
+
+#padding zero upto maximum length
+x_tr    =   pad_sequences(x_tr,  maxlen=max_len_text, padding='post') 
+x_val   =   pad_sequences(x_val, maxlen=max_len_text, padding='post')
+
+x_voc_size   =  len(x_tokenizer.word_index) +1
