890f294c57c8ee3771a9e217b8f7f40591980b72
Somyajit Chakraborty Sam <somyajitchppr@gmail.com>
2019-11-24 18:26:24 +0530

    Create summarytokenizer.py

---
+++ Sam_copy_mech/Code/summarytokenizer.py
@@ -0,0 +1,13 @@
+#preparing a tokenizer for summary on training data 
+y_tokenizer = Tokenizer()
+y_tokenizer.fit_on_texts(list(y_tr))
+
+#convert summary sequences into integer sequences
+y_tr    =   y_tokenizer.texts_to_sequences(y_tr) 
+y_val   =   y_tokenizer.texts_to_sequences(y_val) 
+
+#padding zero upto maximum length
+y_tr    =   pad_sequences(y_tr, maxlen=max_len_summary, padding='post')
+y_val   =   pad_sequences(y_val, maxlen=max_len_summary, padding='post')
+
+y_voc_size  =   len(y_tokenizer.word_index) +1
